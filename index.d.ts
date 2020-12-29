706028e3f4b01604f69077f825312afcec11674d
cutls <web-pro@cutls.com>
2020-12-29 09:44:21 +0900

    add component as img

--- index.d.ts
+++ index.d.ts
@@ -22,6 +22,7 @@ interface OPTIONS {
     maskColor?: string
     maskStyle?: ViewStyle
     imgSource?: ImageSourcePropType
+    componentAsImg?: JSX.Element
     imgStyle?: ImageStyle
     loading?: boolean
     indicatorSize?: [string, number]
